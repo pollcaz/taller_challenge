481938fc1da0c104b7dc98ac325ba088977548a3
Paulo Carmona <pcarmonaz@gmail.com>
2025-04-15 17:42:25 -0500

    Apply migrations for Book and Reservation models

--- db/migrate/20250415223119_create_reservations.rb
+++ db/migrate/20250415223119_create_reservations.rb
@@ -1,7 +1,7 @@
 class CreateReservations < ActiveRecord::Migration[7.2]
   def change
     create_table :reservations do |t|
-      t.string :user_email
+      t.string :user_email, null: false
       t.references :book, null: false, foreign_key: true
 
       t.timestamps
